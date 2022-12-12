57ac040e9f0c369e9f6ad29572f3226ee5d36b77
faraji-fuji <farajifuji@gmail.com>
2022-12-12 08:02:07 +0300

    Age.py, a program that calculates your age from your birth date

---
+++ Python Basics/age.py
@@ -0,0 +1,13 @@
+# A program tha calculates your age
+
+# Get birth year from user
+birth_year = int(input("When were you born? "))
+
+# initialize current year
+current_year = 2022
+
+# calculate age
+age = current_year - birth_year
+
+# print age
+print(f"Your are {age} years old.")
