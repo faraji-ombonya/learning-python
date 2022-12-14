6b43adb4b9c5fce564a7307b982a66e0fa870d49
faraji-fuji <farajifuji@gmail.com>
2022-12-14 20:17:02 +0300

    loops and conditional

---
+++ Python Basics/simple_gui.py
@@ -0,0 +1,19 @@
+#Exercise
+# Use loops and conditional logic to draw a simple gui
+
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+for image in picture:
+    for pixel in image:
+        if pixel == 0:
+            print(" ", end="")
+        else: print("*", end="")
+    print("", end="\n")
+    
