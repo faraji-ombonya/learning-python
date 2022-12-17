6bfa24ef228294ab8dabcf356612fc63a90d12b7
faraji-fuji <farajifuji@gmail.com>
2022-12-17 20:48:21 +0300

    generators

---
+++ Generators/fib.py
@@ -0,0 +1,20 @@
+# fibonacci generator
+def fib(num):
+    a = 0
+    b = 1    
+    for i in range(num + 1):
+        if i == 0:
+            yield 0
+            continue
+        if i == 1:
+            yield 1
+            continue
+        n = a + b
+        a = b
+        b = n 
+        yield n
+
+for i in fib(30):
+    print(i)
+
+
